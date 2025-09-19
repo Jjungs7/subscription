973372f38b69b92d3ac4fa10b211ef9787149e29
ian.lock(양정일)/kakao <ian.lock@kakaocorp.com>
2025-09-19 17:38:12 +0900

    feat: add email send functionalities

--- src/main/kotlin/com/jjungs/subscription/infrastructure/EmailNotificationAdapter.kt
+++ src/main/kotlin/com/jjungs/subscription/infrastructure/EmailNotificationAdapter.kt
@@ -1,4 +1,19 @@
 package com.jjungs.subscription.infrastructure
 
-class EmailNotificationAdapter {
+import com.jjungs.subscription.domain.notification.Notification
+import com.jjungs.subscription.domain.notification.NotificationPort
+import org.springframework.stereotype.Component
+
+@Component
+class EmailNotificationAdapter : NotificationPort {
+    override fun send(notification: Notification) {
+        try {
+            Thread.sleep(100)
+            notification.markAsSent()
+        } catch (e: Exception) {
+            notification.markAsFailed()
+            println(e.message)
+            println(e.stackTrace)
+        }
+    }
 }
