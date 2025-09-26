532dedb96790c87a6bf7769e71769689588adb5f
ian.lock(양정일)/kakao <ian.lock@kakaocorp.com>
2025-09-26 10:21:43 +0900

    feat: add customer repository

--- src/main/kotlin/com/jjungs/subscription/domain/customer/CustomerRepository.kt
+++ src/main/kotlin/com/jjungs/subscription/domain/customer/CustomerRepository.kt
@@ -1,4 +1,12 @@
 package com.jjungs.subscription.domain.customer
 
+import com.jjungs.subscription.domain.vo.Email
+
 interface CustomerRepository {
+    fun save(customer: Customer)
+    fun findById(id: CustomerId): Customer?
+    fun findByEmail(email: Email): Customer?
+    fun findByStatus(status: CustomerStatus): List<Customer>
+    fun findAll(): List<Customer>
+    fun deleteById(id: CustomerId)
 }
