4474513f9882e0a115ba47fff9abd7ee4c149f3c
ian.lock(양정일)/kakao <ian.lock@kakaocorp.com>
2025-10-03 13:22:46 +0900

    chore: name, phone 은 value object 이지만 constructor 문제로 class 로 사용

--- src/main/kotlin/com/jjungs/subscription/domain/customer/Phone.kt
+++ src/main/kotlin/com/jjungs/subscription/domain/customer/Phone.kt
@@ -1,6 +1,8 @@
 package com.jjungs.subscription.domain.customer
 
 class Phone(number: String) {
+    // data class 로 변경하고싶지만, private constructor 를 사용해도 copy() 때문에 constructor 가 노출되는 문제가 있어 factory method 에 제한이 있음
+    // https://youtrack.jetbrains.com/issue/KT-11914/Confusing-data-class-copy-with-private-constructor?_gl=1*1k6ie9g*_gcl_au*OTQ4NDc2ODE5LjE3NTYxMjMxMjU.*_ga*MTkwNTc1MzYxMC4xNzU2MTIzMTI3*_ga_9J976DJZ68*czE3NTk0NjQxNDckbzckZzEkdDE3NTk0NjQ2MzEkajU0JGwwJGgw&_cl=MTsxOzE7SkxXVkhyNk1RNEJZSFIwME01RXdJRHRHejMxd3hjd0lYT1RIRmVMc3Y2d2R6eWVQNUlLVklGRDM5SFZWUGc5Rjs=
     val countryCode: CountryCode
     val number: String
 
